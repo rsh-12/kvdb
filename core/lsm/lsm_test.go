fd13d9051582aab2f8070c377f81e68b8c8b2567
Shukhrat Rakhmonov <get_info@bk.ru>
2025-02-23 13:35:46 +0500

    Refactor lsm tests

--- core/lsm/lsm_test.go
+++ core/lsm/lsm_test.go
@@ -6,20 +6,44 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestGet(t *testing.T) {
-	lsm := NewLSMTree(2)
-	lsm.Put("level", "info")
-	lsm.Put("profile", "dev")
+func TestLsm(t *testing.T) {
+	setUp := func(threshold int, enrich func(*LSMTree)) *LSMTree {
+		lsm := NewLSMTree(threshold)
+		enrich(lsm)
+		return lsm
+	}
 
-	t.Run("value exists", func(t *testing.T) {
+	t.Run("value exists in memtable", func(t *testing.T) {
+		lsm := setUp(10, func(l *LSMTree) {
+			l.Put("level", "debug")
+		})
+
+		value, exists := lsm.Get("level")
+
+		assert.Equal(t, "debug", value)
+		assert.True(t, exists)
+	})
+
+	t.Run("value exists in sstable", func(t *testing.T) {
+		lsm := setUp(1, func(l *LSMTree) {
+			l.Put("level", "info")
+		})
+
 		got, _ := lsm.Get("level")
 		want := "info"
+
 		assert.Equal(t, want, got)
 	})
 
-	t.Run("value doesn't exist", func(t *testing.T) {
-		_, exists := lsm.Get("config")
+	t.Run("value doesn't exist in sstable", func(t *testing.T) {
+		lsm := setUp(1, func(l *LSMTree) {
+			l.Put("level", "info")
+		})
+
+		value, exists := lsm.Get("config")
+
 		assert.False(t, exists)
+		assert.Empty(t, value)
 	})
 
 }
