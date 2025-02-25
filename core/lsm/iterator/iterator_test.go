2a9dbd0e66bd0f86ea462f60fa6121c5b2c0927e
Shukhrat Rakhmonov <get_info@bk.ru>
2025-02-25 11:41:43 +0500

    Refactor iterator tests

--- core/lsm/iterator/iterator_test.go
+++ core/lsm/iterator/iterator_test.go
@@ -4,35 +4,55 @@ import (
 	"kvdb/core/lsm/iterator"
 	"kvdb/core/lsm/memtable"
 	"kvdb/tests"
+	"kvdb/types"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
 func TestSstableIterator(t *testing.T) {
+	path := "/tmp/sstable"
+	memtable := setUp()
+	memtable.Flush(path)
+
+	iter, err := iterator.NewSstableIterator(path)
+
+	assert.Nil(t, err)
+	assertIterator(t, iter, []types.Item{
+		{Key: "level", Value: "info"},
+		{Key: "profile", Value: "staging"},
+	})
+
+	tests.ClearTestData()
+}
+
+func TestMemtableIterator(t *testing.T) {
+	memtable := setUp()
+
+	iter, err := memtable.Iterator()
+
+	assert.Nil(t, err)
+	assertIterator(t, iter, []types.Item{
+		{Key: "level", Value: "info"},
+		{Key: "profile", Value: "staging"},
+	})
+}
+
+func setUp() *memtable.MemTable {
 	memtable := memtable.NewMemTable()
 	memtable.Put("level", "info")
 	memtable.Put("profile", "staging")
-	memtable.Flush("/tmp/sstable")
+	return memtable
+}
 
-	iter, err := iterator.NewSstableIterator("/tmp/sstable")
-	assert.Nil(t, err)
-
-	hasNext := iter.HasNext()
-	assert.True(t, hasNext)
-
-	item, err := iter.Next()
-
-	assert.Nil(t, err)
-	assert.Equal(t, "level", item.Key)
-	assert.Equal(t, "info", item.Value)
-
-	item, err = iter.Next()
-	assert.Nil(t, err)
-	assert.Equal(t, "profile", item.Key)
-	assert.Equal(t, "staging", item.Value)
-
+func assertIterator(t testing.TB, iter types.Iterator, expectedItems []types.Item) {
+	t.Helper()
+	for _, expected := range expectedItems {
+		assert.True(t, iter.HasNext())
+		item, err := iter.Next()
+		assert.Nil(t, err)
+		assert.Equal(t, expected.Key, item.Key)
+		assert.Equal(t, expected.Value, item.Value)
+	}
 	assert.False(t, iter.HasNext())
-
-	tests.ClearTestData()
 }
